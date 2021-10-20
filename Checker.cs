866917c4afa09e2bc41147f7c3d0eecf606b5fcf
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-10-20 23:06:54 +0200

    Add files via upload

---
+++ Checker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_02._08
+{
+    public class Checker
+    {
+        public bool IsEmpty(string str)
+        {
+           if(String.IsNullOrEmpty(str) == true)
+           {
+                return true;
+           }
+           else
+           {
+                return false;
+           }
+        }
+    }
+}
